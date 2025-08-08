4e8a58ee43bd6ed569e71a8a50fcd802445df09e
Kavia AI <production@kavia.ai>
2025-08-08 11:30:46 +0000

    CheckPoint - cg908b179b

--- sports_telecast_frontend/src/components/Header.js
+++ sports_telecast_frontend/src/components/Header.js
@@ -51,12 +51,7 @@ const Header = ({ viewerCount }) => {
             </span>
           </div>
 
-          {/* Search */}
-          <button className="p-2 bg-secondary-bg hover:bg-hover-bg rounded-lg transition-all duration-200 hover-scale group">
-            <svg className="w-5 h-5 text-text-secondary group-hover:text-text-primary" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="m21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
-            </svg>
-          </button>
+          {/* (Search button removed as part of 'Remove Search Feature' task) */}
           
           {/* User Profile */}
           <div className="relative group">
